85d558dc12a1e6cff1e9d62263fa6c485aac3a71
bini739 <umbini213@gmail.com>
2025-05-17 00:53:47 +0300

    Added stracture,null default values and function declaration

--- Music PlayList Manager.cpp
+++ Music PlayList Manager.cpp
@@ -4,3 +4,23 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
+//create the stracture for the nodes
+struct Song { int id; string title, artist; };
+struct Node { Song* song; Node* prev, *next; };
+
+//default value of linked list
+Node *head = nullptr, *tail = nullptr, *current = nullptr;
+bool repeatMode = false;
+const string fileName = "playlist.txt";
+int nextId = 1;
+
+// Function declarations
+void save();
+void load();
+void show();
+void addSong(string title, string artist, bool saveToFile = true);
+void addMultipleSongs();
+void playNext();
+void playPrevious();
+void shuffle();
