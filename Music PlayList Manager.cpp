cc56d3704391fd1e4c2188e7e4e4da4ead11ddd2
hiwi13 <addisabdisa25@gmail.com>
2025-05-17 01:05:04 +0300

    I created the "load" and "save" functions

--- Music PlayList Manager.cpp
+++ Music PlayList Manager.cpp
@@ -24,3 +24,32 @@ void addMultipleSongs();
 void playNext();
 void playPrevious();
 void shuffle();
+
+/* Function implementations 
+   start by Addishiwot Abdisa by creating "save" and "load" functions*/
+void save() {
+    ofstream file(fileName);
+    for (Node* temp = head; temp; temp = temp->next)
+        file << temp->song->id << "|" << temp->song->title << "|" << temp->song->artist << "\n";
+    file.close();
+}
+
+void load() {
+    ifstream file(fileName);
+    string line;
+    while (getline(file, line)) {
+        size_t p1 = line.find('|'), p2 = line.find('|', p1+1);
+        if (p1 == string::npos || p2 == string::npos) continue;
+
+        Song* s = new Song{stoi(line.substr(0, p1)),
+                         line.substr(p1+1, p2-p1-1),
+                         line.substr(p2+1)};
+        Node* newNode = new Node{s, tail, nullptr};
+
+        (head ? tail->next : head) = newNode;
+        tail = newNode;
+        if (s->id >= nextId) nextId = s->id + 1;
+    }
+    current = head;
+    file.close();
+}
